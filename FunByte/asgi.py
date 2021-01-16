2810900b707fbf8094b6dfe86f89c7ef3ad4d586
Anupam96786 <anupam96786@gmail.com>
2021-01-16 19:13:57 +0530

    initial commit

---
+++ FunByte/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for FunByte project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FunByte.settings')
+
+application = get_asgi_application()
