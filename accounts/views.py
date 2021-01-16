896e6d3dfd98b02a7197dcfd0227b3d99daec97c
Anupam96786 <anupam96786@gmail.com>
2021-01-16 23:20:11 +0530

    assets added

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 
 def login(request):
     if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'message': 'Invalid Credentials'})
         return render(request, 'login.html')
