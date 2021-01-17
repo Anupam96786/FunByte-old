d3d8e784e158c1b1710386bb965d8e8ee0d580aa
Anupam96786 <anupam96786@gmail.com>
2021-01-17 06:22:16 +0530

    bug fixing

--- FunByte/views.py
+++ FunByte/views.py
@@ -2,7 +2,4 @@ from django.shortcuts import render, redirect
 
 
 def home(request):
-    if request.user.is_authenticated:
-        return render(request, 'home.html')
-    else:
-        return redirect('login')
+    return render(request, 'home.html')
