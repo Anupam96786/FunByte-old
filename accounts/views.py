0ba8e51a8ac26c747cccf6ff7e8a0e912767dac5
Anupam96786 <anupam96786@gmail.com>
2021-01-17 06:21:40 +0530

    user login done

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 
 
-def login(request):
-    if request.method == 'GET':
-        return render(request, 'login.html')
-    elif request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            return render(request, 'login.html', {'message': 'Invalid Credentials'})
-        return render(request, 'login.html')
+def user_login(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'GET':
+            return render(request, 'login.html')
+        elif request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                return render(request, 'login.html', {'message': 'Invalid Credentials'})
+            return render(request, 'login.html')
+
+
+def user_signup(request):
+    return render(request, 'signup.html')
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
