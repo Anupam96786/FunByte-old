ace6f6d4d2770818fcf494b5d3be4d59bbfd24ce
Anupam96786 <anupam96786@gmail.com>
2021-05-16 02:59:18 +0530

    in memory channel layers added for django channels

--- FunByte/settings.py
+++ FunByte/settings.py
@@ -70,13 +70,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'FunByte.wsgi.application'
 ASGI_APPLICATION = "FunByte.asgi.application"
+# CHANNEL_LAYERS = {
+#     'default': {
+#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
+#         'CONFIG': {
+#             "hosts": [('127.0.0.1', 6379)],
+#         },
+#     },
+# }
 CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
-    },
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
 }
 
 
